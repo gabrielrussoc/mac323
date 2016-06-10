bc70f0d9390f28a462b2ecc4c8259f5a06a94121
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-06-10 11:46:20 -0300

    vai corinthians, sem dividir por 0 agora

--- exs/Macoeze/GreatBattleForTheKingdom.java
+++ exs/Macoeze/GreatBattleForTheKingdom.java
@@ -25,7 +25,6 @@ public class GreatBattleForTheKingdom {
 
         public int compareTo (Vertex o) {
             int x = this.dist - o.dist;
-            x /= x;
             return x;
         }
     }
